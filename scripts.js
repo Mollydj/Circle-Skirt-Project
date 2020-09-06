995da564931d9f94b5727c827c96ba9ea0a8672a
Molly DeAngelis-Jimenez <55717647+Mollydj@users.noreply.github.com>
2020-09-06 15:45:02 -0400

    Implemented bootstart and cleaned up code

--- scripts.js
+++ scripts.js
@@ -1,52 +1,53 @@
 function getWaist() {
-    const waist = Number(document.getElementById("waistMeasure").value);
-    console.log(waist);
-    return waist;
-  }
+  const waist = Number(document.getElementById("waistMeasure").value);
+  console.log(waist);
+  return waist;
+}
+getWaist();
 
+var pi = Math.PI;
+// var wrap = (getWaist * 1.5) / (2 * pi);
+// var full = (getWaist() / (2 * pi));
+// var half = (getWaist() / pi);
+// var quarter = (getWaist() / (pi / 2));
+
+function skirtLength(length){
  
- var pi = Math.PI;
- // var wrap = (getWaist * 1.5) / (2 * pi);
- // var full = (getWaist() / (2 * pi));
- // var half = (getWaist() / pi);
- // var quarter = (getWaist() / (pi / 2));
+console.log(length);
+document.getElementById("returnLength").innerHTML = length + '"';
+//Wrap
+document.getElementById("fabricReq").innerHTML = ((getWaist() * 1.5) / (2 * pi) + length).toFixed(2) + '"';  
+//Full
+document.getElementById("fabricReq").innerHTML = ((getWaist() / (2 * pi)) + length).toFixed(2) + '"';
+//Half
+document.getElementById("fabricReq").innerHTML = ((getWaist() / pi) + length).toFixed(2) + '"';
+//Quarter
+document.getElementById("fabricReq").innerHTML = ((getWaist() / (pi / 2)) + length).toFixed(2) + '"';
  
- function skirtLength(length){
-   
-  console.log(length);
-  document.getElementById("returnLength").innerHTML = length + '"';
-  document.getElementById("fabricReq").innerHTML = ((getWaist() * 1.5) / (2 * pi) + length).tofixed + '"';  document.getElementById("fabricReq").innerHTML = ((getWaist() / (2 * pi)) + length).toFixed(2) + '"';
-  document.getElementById("fabricReq").innerHTML = ((getWaist() / pi) + length).toFixed(2) + '"';
-  document.getElementById("fabricReq").innerHTML = ((getWaist() / (pi / 2)) + length).toFixed(2) + '"';
-   
-  var totalLength = document.getElementById("radius").innerHTML;
-  console.log(totalLength)
- 
- }
- 
- 
-  function skirtFullness(radius){ 
+
+}
+
+
+function skirtFullness(radius){ 
   
-    getWaist();
+  getWaist();
+  
+  
+  if (radius === wrap) {
+    document.getElementById("radius").innerHTML = ((getWaist() * 1.5) / (2 * pi)).toFixed(2) + '"';
+    console.log("wrap");
+  } else if (radius === full) {
+    document.getElementById("radius").innerHTML = (getWaist() / (2 * pi)).toFixed(2) + '"';
+    console.log("full");
     
+  } else if (radius === half) {
+    document.getElementById("radius").innerHTML = (getWaist() / pi).toFixed(2) + '"';
+    console.log("half");
     
-    if (radius === wrap) {
-      document.getElementById("radius").innerHTML = ((getWaist() * 1.5) / (2 * pi)).toFixed(2) + '"';
-      console.log("wrap");
-    } else if (radius === full) {
-      document.getElementById("radius").innerHTML = (getWaist() / (2 * pi)).toFixed(2) + '"';
-      console.log("full");
-      
-    } else if (radius === half) {
-      document.getElementById("radius").innerHTML = (getWaist() / pi).toFixed(2) + '"';
-      console.log("half");
-      
-    } else if (radius === quarter) {
-      document.getElementById("radius").innerHTML = (getWaist() / (pi / 2)).toFixed(2) + '"';
-      console.log("quarter");
-    }
-  // console.log(radius);
-  // document.getElementById("radius").innerHTML = radius.value;
- } 
- 
- 
+  } else if (radius === quarter) {
+    document.getElementById("radius").innerHTML = (getWaist() / (pi / 2)).toFixed(2) + '"';
+    console.log("quarter");
+  }
+// console.log(radius);
+// document.getElementById("radius").innerHTML = radius.value;
+} 
